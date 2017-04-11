ce79ecffc7032d59d3d5477737840a3fa94196b3
Nathaniel T. Hart <hartbeatnt@gmail.com>
2017-04-11 16:40:15 -0700

    laid 12 pictures out in circle around camera

--- dev/app.js
+++ dev/app.js
@@ -1,21 +1,27 @@
 const THREE = require('three')
+import { circleLayout } from './circleLayout'
 
-// set up scene, camera, renderer
+// initialize scene, camera, renderer
 let winW = window.innerWidth;
 let winH = window.innerHeight;
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 30, winW/winH, 1, 1000 );
+const camera = new THREE.PerspectiveCamera( 60, winW/winH, 1, 1000 );
 const renderer = new THREE.WebGLRenderer({antialias:true});
-
 camera.position.z = 4;
 renderer.setClearColor("#ffffff");
 renderer.setSize( winW, winH );
 document.body.appendChild( renderer.domElement );
 
-const geometry = new THREE.PlaneBufferGeometry( 5, 5 );
-const material = new THREE.MeshBasicMaterial( {color: 0x00ffff, side: THREE.DoubleSide} );
-const plane = new THREE.Mesh( geometry, material );
-plane.position.set(0, 0, -10)
-scene.add( plane );
+// create layout for the picture frames
+const positions = circleLayout(20, 12, camera.position);
+positions.forEach(position=>{
+  let { x, y, z } = position;
+  let geometry = new THREE.PlaneBufferGeometry( 10, 10 );
+  let material = new THREE.MeshBasicMaterial( {color: 0x00ffff} );
+  let plane = new THREE.Mesh( geometry, material );
+  plane.position.set(x, y, z);
+  plane.lookAt(camera.position)
+  scene.add(plane);
+})
 
 renderer.render(scene, camera);
---
+++ dev/circleLayout.js
@@ -0,0 +1,17 @@
+// loosely based on Kevin Ngo's A-Frame layout component:
+// github.com/ngokevin/kframe/tree/master/components/layout/
+
+const circleLayout = (radius, quantity, center) => {
+  let positions = [];
+  for (let i = 0; i < quantity; i++) {
+    let radians = i * (2 * Math.PI) / quantity;
+    positions.push({
+      x: center.x + radius * Math.cos(radians),
+      y: center.y,
+      z: center.z + radius * Math.sin(radians)
+    });
+  }
+  return positions;
+}
+
+export { circleLayout }
